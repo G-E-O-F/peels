e1aee041da87d7383f8e9714990d1b9c22813a1a
Will Shown <w@willshown.com>
2014-11-03 23:05:00 -0800

    First commit: started basic stuff for the things.

---
+++ test/index.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2014 Will Shown. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+var assert = require('assert'),
+    chai = require('chai'),
+    expect = chai.expect,
+    _ = require('lodash');
+
+chai.should();
+
+describe('Sphere', function(){
+
+    var Sphere = require('../lib/sphere');
+
+    var s = new Sphere({
+        divisions: 1
+    });
+
+    debugger;
+
+    describe('constructor', function(){
+
+        it('should have 5 sections.', function(){
+
+            return s._Sections.length.should.equal(5);
+
+        });
+
+        // TODO: write more tests.
+
+    });
+
+});
