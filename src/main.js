6c895d26aff455d27b4794b36123f780a9d0e79f
Will Shown <w@willshown.com>
2014-11-24 10:57:59 -0600

    IT'S ALIIIVE~!

--- src/main.js
+++ src/main.js
@@ -1,6 +1,57 @@
-var Peels = require('../../index.js'),
-    three = require('three');
+var _ = require('lodash'),
+    Peels = require('../../index.js'),
+    THREE = require('three');
 
-var sphere = new Peels({divisions: 8});
+(function(){
 
-console.log(sphere.serialize());
+  var s = new Peels({divisions: 4});
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 33, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  var pixelRatio = matchMedia('(-webkit-min-device-pixel-ratio: 2)').matches ? 2 : 1;
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth * pixelRatio, window.innerHeight * pixelRatio );
+
+  var vnf = s.toCG();
+  var geometry = new THREE.Geometry();
+
+  _.each(vnf.vertices, function(vertex){
+    geometry.vertices.push(
+      new THREE.Vector3(vertex.x, vertex.y, vertex.z)
+    );
+  });
+
+  _.each(vnf.faces, function(face){
+    geometry.faces.push(
+      new THREE.Face3(face[0], face[1], face[2])
+    );
+  });
+
+  var material = new THREE.MeshBasicMaterial({
+    color: 0xc6c2b6,
+    wireframe: true
+  });
+
+  var sphere = new THREE.Mesh( geometry, material );
+  scene.add( sphere );
+
+  camera.position.z = 5;
+
+  var render = function () {
+    requestAnimationFrame(render);
+
+    sphere.rotation.x += 0.002;
+    sphere.rotation.y += 0.001;
+    sphere.rotation.z += 0.001;
+
+    renderer.render(scene, camera);
+  };
+
+  document.addEventListener('DOMContentLoaded', function(e){
+    document.body.appendChild( renderer.domElement );
+    render();
+  });
+
+}());
