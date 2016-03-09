45e26a1e93ac7c2a869d1a7934095e4d76882047
Will Shown <w@willshown.com>
2016-03-09 15:36:57 -0800

    Adjusting tests. Updating engine support.

--- test/index.js
+++ test/index.js
@@ -20,20 +20,20 @@
  * SOFTWARE.
  */
 
-var assert = require('assert'),
-    chai = require('chai'),
+var assert         = require('assert'),
+    chai           = require('chai'),
     chaiAsPromised = require("chai-as-promised"),
-    _ = require('lodash'),
-    q = require('q');
+    _              = require('lodash'),
+    q              = require('q');
 
 chai.should();
 chai.use(chaiAsPromised);
 
 describe('Sphere', function () {
 
-  var Sphere = require('../lib/sphere');
-  var validate = require('../lib/sphere/validate');
-  var Field = require('../lib/field');
+  var Sphere    = require('../lib/sphere');
+  var validate  = require('../lib/sphere/validate');
+  var Field     = require('../lib/field');
   var positions = require('../lib/sphere/positions');
 
   // `s` is a sphere that is essentially an icosahedron, good for testing that strange case.
@@ -103,7 +103,7 @@ describe('Sphere', function () {
       });
 
       it('should connect one of the east-northeastern edge’s hexagon’s adjacent fields.', function () {
-        var dy = z._divisions,
+        var dy     = z._divisions,
             eNEHex = z.get(0, dy, 0);
         return dy.should.equal(3) &&
           eNEHex._adjacentFields.length.should.equal(6) &&
@@ -116,8 +116,8 @@ describe('Sphere', function () {
       });
 
       it('should connect one of the southeastern edge’s hexagon’s adjacent fields.', function () {
-        var dy = z._divisions,
-            dx = dy * 2,
+        var dy    = z._divisions,
+            dx    = dy * 2,
             sEHex = z.get(0, dx - 1, 1);
         return dy.should.equal(3) && dx.should.equal(6) &&
           sEHex._adjacentFields.length.should.equal(6) &&
@@ -130,8 +130,8 @@ describe('Sphere', function () {
       });
 
       it('should connect one of the section-internal hexagon’s adjacent fields.', function () {
-        var dy = z._divisions,
-            dx = dy * 2,
+        var dy       = z._divisions,
+            dx       = dy * 2,
             innerHex = z.get(0, dy, 1);
         return dy.should.equal(3) && dx.should.equal(6) &&
           innerHex._adjacentFields.length.should.equal(6) &&
@@ -147,107 +147,60 @@ describe('Sphere', function () {
 
   });
 
-  describe('serialization', function(){
+  describe('serialization', function () {
 
     // `so` is the serialized version of `s`
     var so = {
       divisions: 1,
-      fields: [
+      fields:    [
         {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
       ]
     };
 
-    it('should match the reference serialization.', function(){
-      return s.serialize().should.satisfy(function(ss){ return _.isEqual(ss, so) });
+    it('should match the reference serialization.', function () {
+      return s.serialize().should.satisfy(function (ss) {
+        return _.isEqual(ss, so)
+      });
     });
 
-    describe('validation', function(){
+    describe('validation', function () {
 
-      // `s1` is the serialized version of `s` with the wrong number of sections.
+      // `s1` is the serialized version of `s` with the wrong number of fields.
       var s1 = {
-        north: {},
-        south: {},
         divisions: 1,
-        fields: [
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]]
+        fields:    [
+          {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
         ]
       };
 
       // `s2` is the serialized version of `s` with a bad divisions value.
       var s2 = {
-        north: {},
-        south: {},
-        divisions: 2,
-        fields: [
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]]
+        divisions: -1,
+        fields:    [
+          {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
         ]
       };
 
-      // `s3` is the serialized version of `s` with a missing field.
-      var s3 = {
-        north: {},
-        south: {},
-        divisions: 1,
-        fields: [
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], ['potato']],
-          [[{}], [{}]]
-        ]
-      };
-
-      // `s4` is the serialized version of `s` with an extra field.
-      var s4 = {
-        north: {},
-        south: {},
-        divisions: 1,
-        fields: [
-          [[{}], [{}]],
-          [[{}], [{}]],
-          [[{}], [{}, {}]],
-          [[{}], [{}]],
-          [[{}], [{}]]
-        ]
-      };
-
-      it('should validate well-formed serializations.', function(){
+      it('should validate well-formed serializations.', function () {
         return validate(so).should.be.true;
       });
 
-      it('should not validate serializations with an incongruous divisions index.', function(){
+      it('should not validate serializations with an incongruous divisions index.', function () {
         return validate(s1).should.be.false;
       });
 
-      it('should not validate serializations with missing fields.', function(){
+      it('should not validate serializations with missing fields.', function () {
         return validate(s2).should.be.false;
-      });
-
-      it('should not validate badly-formed serializations.', function(){
-        return validate(s3).should.be.false;
-      });
-
-      it('should not validate serializations with extra fields.', function(){
-        return validate(s4).should.be.false;
       });
 
     });
 
-    describe('de-serialization', function(){
+    describe('de-serialization', function () {
 
       // `haysphereData` is the serialized version of `s` with some data in it
       var haysphereData = {
         divisions: 1,
-        fields: [
+        fields:    [
           {}, {}, {}, {}, {}, {needle: true}, {}, {}, {}, {}, {}, {}
         ]
       };
@@ -256,12 +209,12 @@ describe('Sphere', function () {
         data: haysphereData
       });
 
-      it('should de-serialize data into Sphere instances.', function(){
+      it('should de-serialize data into Sphere instances.', function () {
         return haysphere.get(1, 1, 0).data.needle.should.be.true;
       });
 
-      it('should serialize into the exact same data as input.', function(){
-        return haysphere.serialize().should.satisfy(function(hss){
+      it('should serialize into the exact same data as input.', function () {
+        return haysphere.serialize().should.satisfy(function (hss) {
           return _.isEqual(hss, haysphereData);
         });
       })
@@ -270,54 +223,54 @@ describe('Sphere', function () {
 
   });
 
-  describe('data', function(){
+  describe('data', function () {
 
-      var field = z.get(0, 1, 0);
+    var field = z.get(0, 1, 0);
 
-      it('should be able to get an adjacent field', function(){
-        return field.adjacent(0).should.be.an.instanceof(Field);
-      });
+    it('should be able to get an adjacent field', function () {
+      return field.adjacent(0).should.be.an.instanceof(Field);
+    });
 
-      it('should be able to get all adjacent fields', function(){
-        return field.adjacents().should.be.an.instanceof(Array);
-      });
+    it('should be able to get all adjacent fields', function () {
+      return field.adjacents().should.be.an.instanceof(Array);
+    });
 
-      it('should be able to set data directly when Sphere isn’t iterating.', function(){
-        field.data = { apple: 'strudel' };
-        return field.data.apple.should.equal('strudel');
-      });
+    it('should be able to set data directly when Sphere isn’t iterating.', function () {
+      field.data = {apple: 'strudel'};
+      return field.data.apple.should.equal('strudel');
+    });
 
-      it('should also be able to set data directly when Sphere is on its first iteration.', function(){
-        field.data = { apple: 'cobbler' };
-        z._iteration = { current: 0 };
-        return field.data.apple.should.equal('cobbler');
-      });
+    it('should also be able to set data directly when Sphere is on its first iteration.', function () {
+      field.data   = {apple: 'cobbler'};
+      z._iteration = {current: 0};
+      return field.data.apple.should.equal('cobbler');
+    });
 
-      it('should not be able to set previous data values when Sphere is on its next iteration.', function(){
-        field.data = { apple: 'tart' };
-        z._iteration = { previous: 0, current: 1 };
-        field.data = { apple: 'pie' };
-        return field.data.apple.should.equal('tart');
-      });
+    it('should not be able to set previous data values when Sphere is on its next iteration.', function () {
+      field.data   = {apple: 'tart'};
+      z._iteration = {previous: 0, current: 1};
+      field.data   = {apple: 'pie'};
+      return field.data.apple.should.equal('tart');
+    });
 
-      it('should point to the latest data values when the Sphere starts a new iteration.', function(){
-        z._iteration = { previous: 1, current: 2 };
-        return field.data.apple.should.equal('pie');
-      });
+    it('should point to the latest data values when the Sphere starts a new iteration.', function () {
+      z._iteration = {previous: 1, current: 2};
+      return field.data.apple.should.equal('pie');
+    });
 
   });
 
-  describe('iteration', function(){
+  describe('iteration', function () {
 
-    it('should be able to set every value.', function(){
+    it('should be able to set every value.', function () {
       var d = q.defer();
 
-      s.iterate(function(done){
+      s.iterate(function (done) {
         this.data = 1;
         done();
       }, d.resolve);
 
-      return d.promise.should.eventually.satisfy(function(){
+      return d.promise.should.eventually.satisfy(function () {
 
         return s.get('NORTH').data === 1 && s.get('SOUTH').data === 1 && s.get(0, 0, 0).data === 1;
 
@@ -327,20 +280,20 @@ describe('Sphere', function () {
 
   });
 
-  describe('geometry', function(){
+  describe('geometry', function () {
 
-    var π = Math.PI,
-        L = Math.acos(Math.sqrt(5) / 5),
-        A = 2 * π / 5,
+    var π         = Math.PI,
+        L         = Math.acos(Math.sqrt(5) / 5),
+        A         = 2 * π / 5,
         tolerance = 1e-10;
 
-    describe('trigonometry', function(){
+    describe('trigonometry', function () {
 
-      var north = {
+      var north     = {
             λ: 1e-15,
             φ: π / 2 + 1e-15
           },
-          refFirst = {
+          refFirst  = {
             λ: 0,
             φ: π / 2 - L
           },
@@ -351,60 +304,60 @@ describe('Sphere', function () {
 
       var d_n_f = positions.distance(north, refFirst);
       var d_f_s = positions.distance(refFirst, refSecond);
-      var mid1 = positions.midpoint(refFirst, refSecond);
+      var mid1  = positions.midpoint(refFirst, refSecond);
 
-      it('should calculate distance accurately.', function(){
+      it('should calculate distance accurately.', function () {
         return d_n_f.should.be.closeTo(L, tolerance) &&
-               d_f_s.should.be.closeTo(L, tolerance);
+          d_f_s.should.be.closeTo(L, tolerance);
       });
 
-      it('should interpolate intermediate points accurately.', function(){
+      it('should interpolate intermediate points accurately.', function () {
         var int1 = positions.interpolate(refFirst, refSecond, 2)[0];
 
         return int1.λ.should.be.closeTo(mid1.λ, tolerance) &&
-               int1.φ.should.be.closeTo(mid1.φ, tolerance);
+          int1.φ.should.be.closeTo(mid1.φ, tolerance);
       });
 
     });
 
-    describe('interpolation', function(){
+    describe('interpolation', function () {
 
-      var u = L/3;
+      var u = L / 3;
 
-      describe('pentagons', function(){
+      describe('pentagons', function () {
 
-        it('should calculate the positions for polar fields accurately.', function(){
+        it('should calculate the positions for polar fields accurately.', function () {
 
           return s.get('NORTH')._pos.λ.should.be.closeTo(0, tolerance) &&
-                 s.get('NORTH')._pos.φ.should.be.closeTo(π/2, tolerance) &&
-                 s.get('SOUTH')._pos.λ.should.be.closeTo(0, tolerance) &&
-                 s.get('SOUTH')._pos.φ.should.be.closeTo(π/-2, tolerance);
+            s.get('NORTH')._pos.φ.should.be.closeTo(π / 2, tolerance) &&
+            s.get('SOUTH')._pos.λ.should.be.closeTo(0, tolerance) &&
+            s.get('SOUTH')._pos.φ.should.be.closeTo(π / -2, tolerance);
 
         });
 
-        it('should calculate the positions for tropical fields accurately.', function(){
+        it('should calculate the positions for tropical fields accurately.', function () {
 
-          return s.get(2, 0, 0)._pos.λ.should.be.closeTo(4*π/5, tolerance) &&
-                 s.get(2, 0, 0)._pos.φ.should.be.closeTo(π/2-L, tolerance) &&
-                 s.get(2, 1, 0)._pos.λ.should.be.closeTo(4*π/5 + π/5, tolerance) &&
-                 s.get(2, 1, 0)._pos.φ.should.be.closeTo(π/-2+L, tolerance);
+          return s.get(2, 0, 0)._pos.λ.should.be.closeTo(4 * π / 5, tolerance) &&
+            s.get(2, 0, 0)._pos.φ.should.be.closeTo(π / 2 - L, tolerance) &&
+            s.get(2, 1, 0)._pos.λ.should.be.closeTo(4 * π / 5 + π / 5, tolerance) &&
+            s.get(2, 1, 0)._pos.φ.should.be.closeTo(π / -2 + L, tolerance);
 
         });
 
       });
 
-      describe('hexagons', function(){
+      describe('hexagons', function () {
 
-        describe('edge hexagons', function(){
+        describe('edge hexagons', function () {
 
-          it('should calculate the positions for northern polar edge fields accurately.', function(){
+          it('should calculate the positions for northern polar edge fields accurately.', function () {
 
-            return z.get(2, 1, 0)._pos.λ.should.be.closeTo(4*π/5, tolerance) &&
-              z.get(2, 1, 0)._pos.φ.should.be.closeTo(π/2-L+u, tolerance);
+            return z.get(2, 1, 0)._pos.λ.should.be.closeTo(4 * π / 5, tolerance) &&
+              z.get(2, 1, 0)._pos.φ.should.be.closeTo(π / 2 - L + u, tolerance);
 
           });
 
-          it('should calculate the positions for second edge fields accurately.', function(){
+          it('should calculate the positions for second edge fields accurately.', function () {
 
             var fieldCourse = positions.course(z.get(2, 2, 0)._pos, z.get(2, 1, 1)._pos);
 
@@ -413,7 +366,7 @@ describe('Sphere', function () {
 
           });
 
-          it('should calculate the positions for third edge fields accurately.', function(){
+          it('should calculate the positions for third edge fields accurately.', function () {
 
             var fieldCourse = positions.course(z.get(2, 2, 0)._pos, z.get(2, 2, 1)._pos);
 
@@ -422,7 +375,7 @@ describe('Sphere', function () {
 
           });
 
-          it('should calculate the positions for fourth edge fields accurately.', function(){
+          it('should calculate the positions for fourth edge fields accurately.', function () {
 
             var fieldCourse = positions.course(z.get(2, 2, 0)._pos, z.get(2, 3, 0)._pos);
 
@@ -431,7 +384,7 @@ describe('Sphere', function () {
 
           });
 
-          it('should calculate the positions for fifth edge fields accurately.', function(){
+          it('should calculate the positions for fifth edge fields accurately.', function () {
 
             var fieldCourse = positions.course(z.get(2, 5, 0)._pos, z.get(2, 4, 1)._pos);
 
@@ -440,39 +393,39 @@ describe('Sphere', function () {
 
           });
 
-          it('should calculate the positions for southern polar edge fields accurately.', function(){
+          it('should calculate the positions for southern polar edge fields accurately.', function () {
 
             return z.get(2, 5, 1)._pos.λ.should.be.closeTo(π, tolerance) &&
-              z.get(2, 5, 1)._pos.φ.should.be.closeTo(π/-2+L-u, tolerance);
+              z.get(2, 5, 1)._pos.φ.should.be.closeTo(π / -2 + L - u, tolerance);
 
           });
 
         });
 
-        describe('face hexagons', function(){
+        describe('face hexagons', function () {
 
-          it('should calculate the positions for north polar face fields.', function(){
+          it('should calculate the positions for north polar face fields.', function () {
 
             return z.get(3, 0, 1).should.have.deep.property('_pos.λ') &&
               z.get(3, 0, 1)._pos.λ.should.be.a('number');
 
           });
 
-          it('should calculate the positions for west tropical face fields.', function(){
+          it('should calculate the positions for west tropical face fields.', function () {
 
             return z.get(3, 1, 2).should.have.deep.property('_pos.λ') &&
               z.get(3, 1, 2)._pos.λ.should.be.a('number');
 
           });
 
-          it('should calculate the positions for east tropical face fields.', function(){
+          it('should calculate the positions for east tropical face fields.', function () {
 
             return z.get(3, 3, 1).should.have.deep.property('_pos.λ') &&
               z.get(3, 3, 1)._pos.λ.should.be.a('number');
 
           });
 
-          it('should calculate the positions for south polar face fields.', function(){
+          it('should calculate the positions for south polar face fields.', function () {
 
             return z.get(3, 4, 2).should.have.deep.property('_pos.λ') &&
               z.get(3, 4, 2)._pos.λ.should.be.a('number');
