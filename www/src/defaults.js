5324213507e3c32e8d9596c407943975e0bcf3b4
Will Shown <w@willshown.com>
2016-09-16 03:21:26 +0200

    Updating defaults.

--- www/src/defaults.js
+++ www/src/defaults.js
@@ -26,6 +26,6 @@ export const divisions = 10;
 export const wireframe = false;
 export const wireframeLinewidth = 1.2;
 export const leds = 64;
-export const coloration = 'highlightIcosahedron';
+export const coloration = 'LEDStrings';
 export const useRGBAsset = 'earth.jpeg';
 export const geometryType = 'poly-per-field';
